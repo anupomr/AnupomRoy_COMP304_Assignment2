a5a711c558cfe9ad34a5b9cb5345c3904e561fdf
Anupom Roy <anupomr@yahoo.com>
2018-05-28 00:16:03 -0400

    Image view added

--- app/src/main/java/com/example/anupo/anupomroy_comp304_assignment2/FirstActivity.java
+++ app/src/main/java/com/example/anupo/anupomroy_comp304_assignment2/FirstActivity.java
@@ -1,7 +1,15 @@
 package com.example.anupo.anupomroy_comp304_assignment2;
-
+/*
+ * Author:  Anupom Roy
+ * Subject: Mobile Application Development
+ * date:    May 25, 2018
+ * Professor:Vinayagathas Vaithilingam
+ * Lab:     LabAssignment 2
+ * */
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +17,10 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        // Image for Home Screen
+        BitmapDrawable bmpDrawable=(BitmapDrawable)getResources().getDrawable(R.drawable.electronics);
+        ImageView imageView=(ImageView)findViewById(R.id.mainImageView);
+        imageView.setImageDrawable(bmpDrawable);
     }
 }
